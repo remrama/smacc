a7bea3f7dc8272672514c99c9d92aa0fe1794c29
remrama <mallett.remy@gmail.com>
2023-09-08 14:11:23 -0400

    check for inpout and download if requested

--- smacc/__main__.py
+++ smacc/__main__.py
@@ -3,9 +3,33 @@ import sys
 
 if len(sys.argv) == 1:
     # Run main window.
-    from PyQt5.QtWidgets import QApplication
+    from PyQt5.QtWidgets import QApplication, QMessageBox
     from .gui import SubjectSessionRequest, SmaccWindow
     app = QApplication(sys.argv)
+
+    # Check for the presence of inpout and ask to download if not present.
+    from .utils import inpout_exists, download_inpout
+    if not inpout_exists():
+        inpout_box = QMessageBox()
+        inpout_box.setWindowTitle("Inpout Check")
+        inpout_box.setText("Inpout was not found. This is required to send port codes/triggers.\n\nWould you like to download inpout?")
+        inpout_box.setIcon(QMessageBox.Question)
+        inpout_box.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+        # Show the message box and get the response.
+        response = inpout_box.exec_()
+        if response == QMessageBox.Yes:
+            download_inpout()
+
+        msg_box = QMessageBox()
+        msg_box.setWindowTitle("Information")
+        msg_box.setText("Restart SMACC to utilize port codes/triggers.")
+        msg_box.setIcon(QMessageBox.Information)
+        msg_box.setStandardButtons(QMessageBox.Ok)
+        # Show the message box.
+        msg_box.exec_()
+        sys.exit()
+
+
     inbox = SubjectSessionRequest()
     inbox.exec_()
     if inbox.result():  # 1 if they hit Ok, 0 if cancel
@@ -16,7 +40,7 @@ if len(sys.argv) == 1:
 else:
     from .utils import *
     mode = sys.argv[1]
-    assert mode in ["generate_noise", "generate_cues", "download_inpout"]
+    assert mode in ["generate_noise", "generate_cues"]
     data_directory = get_data_directory()
 
     if mode == "generate_noise":
@@ -57,29 +81,3 @@ else:
         song = seq1 + seq2 + seq3
         export_path = cues_directory / "song.wav"
         write(export_path, 44100, song)
-
-    elif mode == "download_inpout":
-        # Install parallel port driver (optional, only if triggers needed)        
-        import os
-        from pathlib import Path
-        import shutil
-        import urllib.request
-        import zipfile
-        download_path = data_directory / "InpOutBinaries.zip"
-        # Download InpOut32 from https://www.highrez.co.uk/downloads/inpout32
-        url = "https://www.highrez.co.uk/scripts/download.asp?package=InpOutBinaries"
-        urllib.request.urlretrieve(url, download_path)
-        # Extract all files from the zip.
-        unzip_path = download_path.with_suffix("")
-        with zipfile.ZipFile(download_path, "r") as f:
-            f.extractall(unzip_path)
-        # Install the driver.
-        driver_path = unzip_path / "Win32" / "InstallDriver.exe"
-        subprocess.check_call([driver_path], shell=True)
-        # Move the .dll file to Windows/System32/ directory.
-        dll_old_path = unzip_path / "x64" / "inpoutx64.dll"
-        dll_new_path = Path("C:\\Windows\\System32\\")
-        shutil.move(dll_old_path, dll_new_path)
-        # Clean up by deleting unused inpout files.
-        shutil.rmtree(unzip_path)
-        os.remove(download_path)
--- smacc/utils.py
+++ smacc/utils.py
@@ -49,3 +49,35 @@ def brownian_noise(f):
 @PSDGenerator
 def pink_noise(f):
     return 1/np.where(f == 0, float("inf"), np.sqrt(f))
+
+def inpout_exists():
+    dll_path = Path("C:\\Windows\\System32\\inpoutx64.dll")
+    return dll_path.exists()
+
+def download_inpout():
+    # Install parallel port driver (optional, only if triggers needed)        
+    from .utils import get_data_directory
+    data_directory = get_data_directory()
+    import os
+    import shutil
+    import subprocess
+    import urllib.request
+    import zipfile
+    download_path = data_directory / "InpOutBinaries.zip"
+    # Download InpOut32 from https://www.highrez.co.uk/downloads/inpout32
+    url = "https://www.highrez.co.uk/scripts/download.asp?package=InpOutBinaries"
+    urllib.request.urlretrieve(url, download_path)
+    # Extract all files from the zip.
+    unzip_path = download_path.with_suffix("")
+    with zipfile.ZipFile(download_path, "r") as f:
+        f.extractall(unzip_path)
+    # Install the driver.
+    driver_path = unzip_path / "Win32" / "InstallDriver.exe"
+    subprocess.check_call(driver_path)
+    # Move the .dll file to Windows/System32/ directory.
+    dll_old_path = unzip_path / "x64" / "inpoutx64.dll"
+    dll_new_path = Path("C:\\Windows\\System32\\")
+    shutil.move(dll_old_path.as_posix(), dll_new_path.as_posix())
+    # Clean up by deleting unused inpout files.
+    shutil.rmtree(unzip_path.as_posix())
+    os.remove(download_path.as_posix())
